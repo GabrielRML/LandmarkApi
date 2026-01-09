5da989673b49443a0be1ee74cc649ddc2a1e82bf
Gabriel Lima <gabrielmirandaricardo@gmail.com>
2026-01-09 09:02:32 -0300

    feat: added the entities City, State, and TouristPoint.

---
+++ Domain/Entities/TouristPoint.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace LandmarkApi.Domain.Entities;
+
+public class TouristPoint
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    [MaxLength(100)]
+    public string Description { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+    public int CityId { get; set; }
+    public City City { get; set; } = null!;
+}
